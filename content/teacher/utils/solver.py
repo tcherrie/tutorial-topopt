3bf7a3de221adfe5bdff4e8ac043eb20939f66b9
tcherrie <72595712+tcherrie@users.noreply.github.com>
2026-03-09 19:19:15 +0100

    update notebook 1

--- content/teacher/utils/solver.py
+++ content/teacher/utils/solver.py
@@ -1,13 +1,14 @@
-from netgen.geom2d import SplineGeometry
+from ngsolve import GridFunction, LinearForm, BilinearForm
+from ngsolve.la import BaseMatrix
 
-
-from ngsolve import GridFunction
-
-def solve(bilinearForm, linearForm):
+def solve(bilinearForm :LinearForm, 
+          linearForm : BilinearForm
+          ) -> tuple[GridFunction, BaseMatrix]:
+    """ Solve the linear system associated to the given bilinear and linear forms."""
     K = bilinearForm.Assemble().mat
     f = linearForm.Assemble().vec
     fes = bilinearForm.space
     sol = GridFunction(fes)
     Kinv = K.Inverse(freedofs = fes.FreeDofs())
     sol.vec.data = Kinv * f
-    return sol
+    return sol, Kinv
